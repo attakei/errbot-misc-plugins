459fb1a7f304c7fc20ce976c93621cd9315ef52d
Kazuya Takei <attakei@gmail.com>
2017-06-25 02:11:51 +0900

    Define configure items

--- iijmio/iijmio.py
+++ iijmio/iijmio.py
@@ -1,4 +1,5 @@
-from errbot import BotPlugin, botcmd, arg_botcmd, webhook
+# -*- coding:utf8 -*-
+from errbot import BotPlugin, botcmd
 
 
 class Iijmio(BotPlugin):
@@ -6,91 +7,11 @@ class Iijmio(BotPlugin):
     IIJmio monitor
     """
 
-    def activate(self):
-        """
-        Triggers on plugin activation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Iijmio, self).activate()
-
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Iijmio, self).deactivate()
-
     def get_configuration_template(self):
-        """
-        Defines the configuration structure this plugin supports
+        return {
+            'username': "YOUR_IIJMIO_USERNAME",
+            'password': "YOUR_IIJMIO_PASSWORD", }
 
-        You should delete it if your plugin doesn't use any configuration like this
-        """
-        return {'EXAMPLE_KEY_1': "Example value",
-                'EXAMPLE_KEY_2': ["Example", "Value"]
-               }
-
-    def check_configuration(self, configuration):
-        """
-        Triggers when the configuration is checked, shortly before activation
-
-        Raise a errbot.utils.ValidationException in case of an error
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Iijmio, self).check_configuration(configuration)
-
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    @webhook
-    def example_webhook(self, incoming_request):
-        """A webhook which simply returns 'Example'"""
+    @botcmd(name='iijmio_current')
+    def fetch_currently_usage(self, message, args):
         return "Example"
-
-    # Passing split_args_with=None will cause arguments to be split on any kind
-    # of whitespace, just like Python's split() does
-    @botcmd(split_args_with=None)
-    def example(self, message, args):
-        """A command which simply returns 'Example'"""
-        return "Example"
-
-    @arg_botcmd('name', type=str)
-    @arg_botcmd('--favorite-number', type=int, unpack_args=False)
-    def hello(self, message, args):
-        """
-        A command which says hello to someone.
-
-        If you include --favorite-number, it will also tell you their
-        favorite number.
-        """
-        if args.favorite_number is None:
-            return "Hello {name}".format(name=args.name)
-        else:
-            return "Hello {name}, I hear your favorite number is {number}".format(
-                name=args.name,
-                number=args.favorite_number,
-            )
